d4b11cf0857d4a46f4fdd07efd7f3c92ba50dad5
yasuo11 <3251497939@qq.com>
2019-02-01 21:40:09 +0800

    修改配置

--- src/main/java/com/xiuchu/kkfcc/KkfccApplication.java
+++ src/main/java/com/xiuchu/kkfcc/KkfccApplication.java
@@ -1,8 +1,15 @@
 package com.xiuchu.kkfcc;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
+//扫描 mybatis mapper 包路径
+@MapperScan(basePackages = "com.xiuchu.kkfcc.mapper")
+//扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages= {"com.xiuchu.kkfcc"})
 @SpringBootApplication
 public class KkfccApplication {
 
