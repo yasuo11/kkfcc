793fc75990c58c6c5770aafce7e84ebe152846d1
true-wangxu <w_w_w_x_x_x@163.com>
2019-04-11 15:00:07 +0800

    未登录拦截器

---
+++ src/main/resources/static/js/userImg.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+    user_photo();
+});
+
+function user_photo() {
+    $.ajax({
+        url: '/user/userPhoto.do',
+        data:{
+        },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        type: 'GET',
+        success: function(result) {
+            if(result.success) {
+                $("#photo").attr("src", result.data);
+            }else {
+                alert("请先登录！");
+                window.location.href = "/login";
+            }
+        },
+        error: function () {
+            alert("异常错误！")
+        }
+    })
+}
+
