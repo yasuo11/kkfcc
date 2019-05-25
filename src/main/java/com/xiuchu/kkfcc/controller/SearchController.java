3644d06b37ae9d9c42ed4e8564eb236b61feb17b
yasuo11 <3251497939@qq.com>
2019-05-25 22:22:30 +0800

    打

--- src/main/java/com/xiuchu/kkfcc/controller/SearchController.java
+++ src/main/java/com/xiuchu/kkfcc/controller/SearchController.java
@@ -10,7 +10,9 @@ import com.xiuchu.kkfcc.service.IUserService;
 import com.xiuchu.kkfcc.vo.QRecipeVO;
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.segmentation.Word;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,8 +43,11 @@ public class SearchController {
     @RequestMapping("/hint.do")
     @ResponseBody
     public List<String> searchHint(String keyWord) {
-        WildcardQueryBuilder queryBuilder = QueryBuilders.wildcardQuery("recipeName", "*" + keyWord + "*");
-        EsPage esPage = ElasticsearchUtil.search(queryBuilder, "recipe", "info", 0, 10, "recipeName,id", null, "recipeName");
+        BoolQueryBuilder qbs = QueryBuilders.boolQuery();
+        QueryBuilder qb1 = QueryBuilders.matchPhraseQuery("recipeName", keyWord);
+        BoolQueryBuilder bqb1 = QueryBuilders.boolQuery().must(qb1);
+        qbs.must(bqb1);
+        EsPage esPage = ElasticsearchUtil.search(qbs, "recipe", "info", 0, 10, "recipeName,id", null, "recipeName");
 
         List<Map<String, Object>> recordList = esPage.getRecordList();
         List<String> res = new ArrayList<>();
@@ -58,8 +63,11 @@ public class SearchController {
         List<Word> words = WordSegmenter.seg(keyWord);
 
         for(Word word : words) {
-            WildcardQueryBuilder queryBuilder = QueryBuilders.wildcardQuery("recipeName", "*" + word.getText() + "*");
-            EsPage esPage = ElasticsearchUtil.search(queryBuilder, "recipe", "info", 0, 10, "recipeName,id", null, "recipeName");
+            BoolQueryBuilder qbs = QueryBuilders.boolQuery();
+            QueryBuilder qb1 = QueryBuilders.matchPhraseQuery("recipeName", word.getText());
+            BoolQueryBuilder bqb1 = QueryBuilders.boolQuery().must(qb1);
+            qbs.must(bqb1);
+            EsPage esPage = ElasticsearchUtil.search(qbs, "recipe", "info", 0, 10, "recipeName,id", null, "recipeName");
             List<Map<String, Object>> recordList = esPage.getRecordList();
             for(Map<String, Object> map : recordList) {
                 QRecipeVO qRecipeVO = new QRecipeVO();
